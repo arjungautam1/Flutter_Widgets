c9381ff02da2f706c0d5feba2077bd636c4a4336
arjun-sudo <laserarjun@gmail.com>
2020-10-23 15:36:32 +0545

    container widget

---
+++ android/app/src/main/kotlin/com/laser/flutter_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.laser.flutter_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Container example"),
+        ),
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.all(10.0),
+            color: Colors.amber[600],
+            width: 48.0,
+            height: 48.0,
+          ),
+        )
+        ),
+      );
+  }
+}
